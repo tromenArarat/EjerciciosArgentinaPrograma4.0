8aaa09b4e73ca4958bcaec9bd5886af5828feb0b
Tomas Manoukian <tomanoukian@gmail.com>
2023-03-15 16:55:15 -0300

    terceras

--- EjerciciosGuia3/src/ejerciciosguia3/Ejercicio6extra.java
+++ EjerciciosGuia3/src/ejerciciosguia3/Ejercicio6extra.java
@@ -10,28 +10,33 @@ import java.util.Scanner;
 public class Ejercicio6extra {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in).useDelimiter("\n");
         System.out.println("¿Cuántos sujetos va a medir?");
         int medidos = sc.nextInt();
         int suma = 0;
         int sumaSopetis = 0;
+        double sumaAlturaSope = 0;
+        double sumaAlturaNoSope = 0;
         double sumaAlturas = 0;
         double promedioGral = 0;
         double promedioSopetis = 0;
-        for (int i = 1; i == medidos; i++) {
+        for (int i = 0; i < medidos; i++) {
             System.out.println("Ingrese altura:");
             double altura = sc.nextDouble();
             if(altura<1.60){
+                sumaAlturaSope+=altura;
                 sumaSopetis++;
                 }else {
+                sumaAlturaNoSope+=altura;
                 suma++;
             }
-            sumaAlturas = sumaAlturas + altura;
-            promedioGral = medidos/sumaAlturas;
-            promedioSopetis = medidos/sumaSopetis;
+            sumaAlturas = sumaAlturaSope + sumaAlturaNoSope;
+            promedioGral = (sumaAlturas*100)/medidos;
+            promedioSopetis = (medidos*100)/sumaSopetis;
             
         }
-        System.out.println("El promedio de personas que miden menos de 1.6 es "+ promedioSopetis +" y el promedio general de estatura es "+ promedioGral);
+        System.out.println("El promedio de personas que miden menos de 1.6 es "+ promedioSopetis);
+        System.out.println("y el promedio general de estatura es "+ promedioGral);
     }
     
 }
