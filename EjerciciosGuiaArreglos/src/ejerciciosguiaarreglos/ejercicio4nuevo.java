b3a217d5de5d8cc5d291d4eaf16b9d42a2a2e735
Tomas Manoukian <tomanoukian@gmail.com>
2023-03-22 16:58:20 -0300

    primeras

--- EjerciciosGuiaArreglos/src/ejerciciosguiaarreglos/ejercicio4nuevo.java
+++ EjerciciosGuiaArreglos/src/ejerciciosguiaarreglos/ejercicio4nuevo.java
@@ -1,21 +1,39 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Los profesores del curso de programación de Egg necesitan llevar un
+registro de las notas adquiridas por sus 10 alumnos para luego obtener
+una cantidad de aprobados y desaprobados. Durante el período de
+cursado cada alumno obtiene 4 notas, 2 por trabajos prácticos
+evaluativos y 2 por parciales. Las ponderaciones de cada nota son:
+Primer trabajo práctico evaluativo 10%
+Segundo trabajo práctico evaluativo 15%
+Primer Integrador 25%
+Segundo integrador 50%
+Una vez cargadas las notas, se calcula el promedio y se guarda en el
+arreglo. Al final del programa los profesores necesitan obtener por
+pantalla la cantidad de aprobados y desaprobados, teniendo en cuenta
+que solo aprueban los alumnos con promedio mayor o igual al 7 de sus
+notas del curso
  */
 package ejerciciosguiaarreglos;
 
-/**
- *
- * @author Tomás
- */
 public class ejercicio4nuevo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[][] notas=new int[10][4];
+        int[] promedio=new int[notas.length];
+        
+// Cómo buscar la longitud de una matriz? .length para la 1era dimension
+// y [0].length para la segunda.
+// línea de comentario
+        for (int i = 0; i < notas.length; i++) {
+            for (int j = 0; j < notas[0].length; j++) {
+                notas[i][j] = (int)(Math.random() *10);
+            }
+        }
+        
+        
+        
+        
     }
     
 }
