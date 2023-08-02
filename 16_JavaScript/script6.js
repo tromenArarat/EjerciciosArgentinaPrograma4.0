119d9817f8b8ba37ec9c2ab89fde97c4d009035e
Tomas Manoukian <tomanoukian@gmail.com>
2023-08-02 14:52:25 -0300

    jseeese

---
+++ 16_JavaScript/script6.js
@@ -0,0 +1,33 @@
+var min = 0;
+var max = 10;
+function getRandomNumber(min,max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var mensaje = " ";
+var numAzar = 0;
+function numRandom(){
+ numAzar = getRandomNumber(min,max);
+document.getElementById("numRandom").setAttribute("value",numAzar);   
+}
+
+document.getElementById("comprob").onclick = parOimpar;
+    
+console.log(`Valor ingresado: ${numAzar}`);
+
+function parOimpar(){
+    
+    if((parseInt(numAzar))==0){
+        mensaje = "el número no es par ni impar";   
+    }else if((parseInt(numAzar))%2!=0){
+        mensaje = "el número es impar";
+    }else if((parseInt(numAzar))%2==0){
+        mensaje = "el número es par";
+    }
+
+    window.alert(mensaje);
+}
+
+
+
+
---
+++ 16_JavaScript/script7.js
@@ -0,0 +1,69 @@
+/*
+Escriba un programa en el cual se ingrese 
+un valor límite positivo(VLP) y, a 
+continuación, solicite números al usuario 
+hasta que la suma de los números 
+introducidos supere 
+el límite inicial.
+*/
+
+// Traigo los elementos que voy a modificar
+const boton = document.getElementById("b1");
+const numField = document.getElementById("numField");
+const label = document.getElementById("lb");
+const numSum = document.getElementById("numSum");
+const sumaField = document.getElementById("suma");
+boton.onclick = llamada;
+var suma = sumaField.value = 0;
+
+// Generación de un número random para simular el VLP
+function llamada() {
+    let min = 10;
+    let max = 100;
+    let VLP = Math.floor(Math.random() * (max - min + 1)) + min;
+    numField.setAttribute("value", VLP);
+    boton.innerHTML = `JUEGUE`;
+    // boton.disabled = true;
+};
+
+// Función para manejar la suma de los números ingresados
+function onEnterKeyPressed() {
+    console.log(suma);
+    const inputValue = parseInt(numSum.value);
+    suma = suma + inputValue;
+    numSum.value = "";
+    label.innerHTML = `Ingrese otro número`;
+    if (suma > parseInt(numField.value)) {
+        label.innerHTML = `La suma supera el límite`;
+    }
+}
+
+// Agregar el evento al campo de entrada para detectar la tecla Enter
+numSum.addEventListener('keydown', handleEnterKey);
+
+function handleEnterKey(event) {
+    if (event.key === 'Enter') {
+        onEnterKeyPressed();
+    }
+}
+
+
+
+// Hacer un append img al final con un tipito haciendo pulgar arriba
+
+
+
+
+
+
+// const textInput = document.getElementById('numSum');
+
+// function onEnterKeyPressed() {
+//   const inputValue = textInput.value;
+//   console.log('Enter key pressed. Input value:', inputValue);
+// }
+
+// function handleEnterKey(event) {
+//   if (event.key === 'Enter') {
+//     onEnterKeyPressed();
+//   };
