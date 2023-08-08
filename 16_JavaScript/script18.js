f745ab247a0622d38bdb5b5000a3741968d07115
Tomas Manoukian <tomanoukian@gmail.com>
2023-08-08 15:00:56 -0300

    sigueJS

--- 16_JavaScript/script18.js
+++ 16_JavaScript/script18.js
@@ -1,13 +1,50 @@
 /*
-A partir del siguiente array: var valores = [true, 5, false, "hola", "adios", 2]:
-a) Determinar cual de los dos elementos de texto es mayor
-b) Utilizando exclusivamente los dos valores booleanos del array, determinar los
-operadores necesarios para obtener un resultado true y otro resultado false
-c) Determinar el resultado de las cinco operaciones matemáticas realizadas con los
-dos elementos numéricos
+A partir del siguiente array: 
+
+var valores = [true, 5, false, "hola", "adios", 2]:
+
+a) Determinar cual de los dos elementos de texto es mayor.
+*/
+var valores = [true, 5, false, "hola", "adios", 2];
+
+var txtMayor = "a";
+for(v of valores){
+    if(typeof v === "string"){
+        if(v.length>txtMayor.length){
+            txtMayor = v;
+        }
+    }
+}
+console.log(txtMayor);
+
+/*
+b) Utilizando exclusivamente los dos valores booleanos 
+del array, determinar los operadores necesarios para 
+obtener un resultado true y otro resultado false.
+*/
+for(v of valores){
+    if(typeof v === "boolean"){
+        v = !v;
+        console.log(v);
+    }
+};
+
+/*
+c) Determinar el resultado de las cinco operaciones 
+matemáticas realizadas con los dos elementos numéricos.
 */
 
-var arr = [1,2,3,4,5];
-arr.pop();
-arr.pop();
-console.log(arr);
+var numeros =[];
+for(v of valores){
+    if(typeof v === "number"){
+        numeros.push(v);
+    }
+};
+
+
+console.log(`El resultado de sumar ${numeros[0]} más ${numeros[1]} es ${numeros[0]+numeros[1]}`);
+console.log(`El resultado de restar ${numeros[0]} menos ${numeros[1]} es ${numeros[0]-numeros[1]}`);
+console.log(`El resultado de multiplicar ${numeros[0]} por ${numeros[1]} es ${numeros[0]*numeros[1]}`);
+console.log(`El resultado de dividir ${numeros[0]} por ${numeros[1]} es ${numeros[0]/numeros[1]}`);
+console.log(`El resto de la división entre ${numeros[0]} y ${numeros[1]} es ${numeros[0]%numeros[1]}`);
+
