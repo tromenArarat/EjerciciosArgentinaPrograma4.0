9739a5e1e4040d2cc56f138e93112b72a5c7b51f
Tomas Manoukian <tomanoukian@gmail.com>
2023-10-06 00:55:54 -0300

    PFinalEtapa3desarrolloDeLaLogica

--- 18_Spring/ServicioSaludo/src/main/java/com/cg/servicioSalud/controladores/TurnoControlador.java
+++ 18_Spring/ServicioSaludo/src/main/java/com/cg/servicioSalud/controladores/TurnoControlador.java
@@ -9,13 +9,16 @@ import com.cg.servicioSalud.entidades.Profesional;
 import com.cg.servicioSalud.entidades.Turno;
 import com.cg.servicioSalud.servicios.ProfesionalServicio;
 import com.cg.servicioSalud.servicios.TurnoServicio;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,9 +33,13 @@ public class TurnoControlador {
 
     // FALTA un requestParam con la fecha del turno seleccionado
     @GetMapping("/confirmado/{id}")
-    public String registrarTurno(@PathVariable String id, HttpSession session, ModelMap modelo) throws Exception {
+    public String registrarTurno(@PathVariable String id, @RequestParam("fecha") String fechaStr, HttpSession session, ModelMap modelo) throws Exception {
+        // Parse the 'fecha' parameter into a Date object
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date fecha = dateFormat.parse(fechaStr);
+        
         Turno turno = new Turno();
-        turno.setFecha(new Date());
+        turno.setFecha(fecha);
         Paciente paciente = (Paciente) session.getAttribute("paciente");
         turno.setPaciente(paciente);
         
@@ -42,10 +49,9 @@ public class TurnoControlador {
         System.out.println(profesional);
         turnoServicio.confirmarTurno(turno);
         
-        
-        modelo.addAttribute("turno",turno);
+        modelo.addAttribute("turno", turno);
         
         return "turno_confirmado.html";
-    }
+}
     
 }
